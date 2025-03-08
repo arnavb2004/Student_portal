145fc9e1ceb0190efe9e8d8efa94b2de57c00945
Arnav Bansal <154666778+arnavb2004@users.noreply.github.com>
2025-03-08 22:48:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://guest-house-psi.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://aims-portal-nine.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
