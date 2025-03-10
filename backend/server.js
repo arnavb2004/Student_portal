9d204ceec5e69b7d9bffcc8dc938b280b0526345
Arnav Bansal <154666778+arnavb2004@users.noreply.github.com>
2025-03-10 19:29:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -385,7 +385,7 @@ app.patch('/api/enrollments/rejectByInstructor/:_id', async (req, res) => {
 });
 
 app.get('/api/enrollments/studentfind', async (req, res) => {
-  const  studentEmai  = req.params.studentEmail;
+  const  studentEmai  = req.query;
   console.log('Received email:', studentEmai);
 
   try {
