74c1d10e5bfe23b01d1123b13ef22685872cd985
Arnav Bansal <154666778+arnavb2004@users.noreply.github.com>
2025-03-10 19:46:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -286,7 +286,7 @@ app.post('/api/enroll', async (req, res) => {
 // In your server code, add the following route to drop the course
 
 app.delete('/api/enrollments/drop', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.query.studentEmail;
 
   try {
     // Find the enrollment by ID
